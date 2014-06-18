a828ec49417474c5b15063320d8f045e63c8e42b
zhlkkk <zhaolongkai3k@gmail.com>
2014-06-19 07:13:12 +0800

    部分页面

--- src/main/java/com/manhattan/controller/MainController.java
+++ src/main/java/com/manhattan/controller/MainController.java
@@ -8,7 +8,9 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -25,15 +27,16 @@ public class MainController {
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public @ResponseBody JsonResult login(@RequestBody User user) {
-        String passwordMd5= DigestUtils.md5Hex(user.getPassword());
-        user=userService.findUserIdByFilter(user.getMobile(), passwordMd5);
+    public @ResponseBody JsonResult login(@RequestParam String mobile,@RequestParam String password) {
+        String passwordMd5= DigestUtils.md5Hex(password);
+        User user=userService.findUserIdByFilter(mobile, passwordMd5);
         JsonResult jsonResult=new JsonResult();
         jsonResult.setSuccess(user!=null);
+        jsonResult.setData(user);
         return jsonResult;
     }
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    @RequestMapping(value = "/register",method = RequestMethod.POST,consumes="application/x-www-form-urlencoded")
     public @ResponseBody JsonResult register(@RequestBody User user){
         String passwordMd5= DigestUtils.md5Hex(user.getPassword());
         user.setPassword(passwordMd5);
@@ -41,5 +44,15 @@ public class MainController {
         JsonResult jsonResult=new JsonResult();
         jsonResult.setSuccess(user!=null);
         return jsonResult;
+    }
+
+    @RequestMapping(value = "/main")
+    public ModelAndView main(@RequestParam(required = false) String userId) {
+        ModelAndView view = new ModelAndView();
+        if (StringUtils.isNotEmpty(userId)) {
+            view.addObject("user", userService.load(userId));
+        }
+        view.setViewName("views/main");
+        return view;
     }
 }
