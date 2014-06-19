04509b426aaeb550dead3fd98b48861d113620a0
wsjyz <wsjyz@aliyun.com>
2014-06-19 14:25:17 +0800

    移动作业相关接口到HomeworkController

--- src/main/java/com/manhattan/controller/HomeworkController.java
+++ src/main/java/com/manhattan/controller/HomeworkController.java
@@ -5,9 +5,7 @@ import com.manhattan.domain.HomeworkSubmit;
 import com.manhattan.domain.Information;
 import com.manhattan.service.HomeWorkService;
 import com.manhattan.service.HomeworkSubmitService;
-import com.manhattan.util.FastJson;
-import com.manhattan.util.OpenPage;
-import com.manhattan.util.PageConvert;
+import com.manhattan.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,8 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by dam on 14-6-18.
@@ -30,6 +32,8 @@ public class HomeworkController {
     private HomeWorkService homeWorkService;
     @Autowired
     private HomeworkSubmitService homeworkSubmitService;
+    @Autowired
+    private ConfigurationFile confBean;
     /**
      * 获取指定学生的作业列表
      * @return
@@ -76,8 +80,29 @@ public class HomeworkController {
      */
     @RequestMapping(value = "/submitHomeWork")
     @ResponseBody
-    public HomeworkSubmit submitHomeWork(@FastJson HomeworkSubmit homeworkSubmit,HttpServletResponse response){
-        HomeworkSubmit homeworkSubmit1=homeworkSubmitService.submit(homeworkSubmit);
+    public HomeworkSubmit submitHomeWork(MultipartHttpServletRequest request,HttpServletResponse response){
+
+        String path = confBean.getFileUploadPath()+request.getParameter("homeworkId");
+
+        MultipartFile file = request.getFile("homeworkFile");
+        String originalFilename = file.getOriginalFilename();
+        String fileSuffix = originalFilename.substring(originalFilename.indexOf("."),originalFilename.length());
+        File targetFile = new File(path, UUIDGen.genShortPK()+fileSuffix);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        try {
+            file.transferTo(targetFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HomeworkSubmit hs = new HomeworkSubmit();
+        hs.setUserId(request.getParameter("userId"));
+        hs.setHomeworkId(request.getParameter("homeworkId"));
+        hs.setHomeworkContent(request.getParameter("homeworkContent"));
+        hs.setOriginalFileName(originalFilename);
+
+        HomeworkSubmit homeworkSubmit1=homeworkSubmitService.submit(hs);
         if (homeworkSubmit1==null) {
             setResponse("作业提交失败", response);
         }
